c621d1e897f86018df04d40f34bc3793d2765898
Nitieii <tuananhngo2513@gmail.com>
2023-08-23 10:50:06 +0700

    change the download id

--- preprocess/__init__.py
+++ preprocess/__init__.py
@@ -7,7 +7,7 @@ from pdf_preprocess import preprocess_pdf
 current_dir = os.path.dirname(os.path.realpath(__file__))
 mrb_ai_dir = os.path.dirname(current_dir)
 
-download_id = "123"
+download_id = sys.argv[1]
 
 # Set up the directories
 raw_pdf_dir = mrb_ai_dir + "/files/" + download_id
