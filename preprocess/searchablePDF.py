c972b87d142da3999b0c2514f00f87495a2949e4
nitieii <tuananhngo2513@gmail.com>
2023-10-22 23:30:51 +0700

    add searchpdf function

--- preprocess/searchablePDF.py
+++ preprocess/searchablePDF.py
@@ -1,21 +1,38 @@
+import os
+
 from pypdf import PdfMerger
 import pytesseract
+from pdf2image import convert_from_path
 
 
-def create_searchable_pdf(image, output_path: str, page_num: int, merger: PdfMerger):
+def create_searchable_pdf(input_path: str):
     """Generate a searchable PDF from document images.
     """
+    merger = PdfMerger()
+    pages = convert_from_path(input_path, thread_count=4)
 
-    # Convert the image to a PDF page
-    pdf_page = pytesseract.image_to_pdf_or_hocr(image, extension='pdf', lang='vie+eng')
-    # Save the PDF page
-    pdf_page_path = f"/tmp/{page_num}.pdf"
-    with open(pdf_page_path, "wb") as f:
-        f.write(pdf_page)
+    file_name = os.path.splitext(os.path.basename(input_path))[0]
 
-    # Append the PDF page to the merger
-    merger.append(pdf_page_path)
-    # os.remove(pdf_page_path)
+    for page_num in range(len(pages)):
+        image = pages[page_num]
 
-    print("Done creating searchable!")
-    return None
+        pdf_page = pytesseract.image_to_pdf_or_hocr(image, extension='pdf', lang='vie+eng', config="--oem 3")
+        # convert the processed images to searchable PDF
+        # Save the PDF page
+        pdf_page_path = f"/tmp/{page_num}.pdf"
+        with open(pdf_page_path, "wb") as f:
+            f.write(pdf_page)
+        # Append the PDF page to the merger
+        merger.append(pdf_page_path)
+        os.remove(pdf_page_path)
+
+        print("Done creating searchable for page: !" + str(page_num))
+
+    if not os.path.exists("ocr"):
+        os.makedirs("ocr", exist_ok=True)
+
+    # convert the processed images to searchable PDF
+    searchable_pdf_dir_with_name = os.path.join("ocr", file_name + ".pdf").replace("\\", "/")
+
+    merger.write(searchable_pdf_dir_with_name)
+    merger.close()
