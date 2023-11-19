81794108b2f5786a6cc3565122a05d53403ace3c
nitieii <tuananhngo2513@gmail.com>
2023-11-19 17:28:37 +0700

    Prevent the application to take all CPU usage.

--- preprocess/pdf_preprocess.py
+++ preprocess/pdf_preprocess.py
@@ -11,7 +11,7 @@ Image.MAX_IMAGE_PIXELS = None
 # Function to get CPU usage percentage
 
 # Set the maximum allowed CPU usage percentage
-max_cpu_usage = 90  # Adjust this value as needed
+max_cpu_usage = 75  # Adjust this value as needed
 
 
 def get_cpu_usage():
@@ -61,61 +61,58 @@ def preprocess_page(page_num, image, processed_dir_path, extracted_dir_path):
         return None
 
 
-def preprocess_pdf(pdf_path, processed_pdf_dir, extracted_text_dir, searchable_pdf_dir):
-    # Extract the file name and create the corresponding directories
-    file_name = os.path.splitext(os.path.basename(pdf_path))[0]
-    processed_dir_path_before = os.path.join(processed_pdf_dir, "before", file_name).replace("\\", "/")
-    processed_dir_path_after = os.path.join(processed_pdf_dir, "after", file_name).replace("\\", "/")
-    extracted_dir_path = os.path.join(extracted_text_dir, file_name).replace("\\", "/")
+def preprocess_pdf(pdf_path, processed_pdf_dir, extracted_text_dir, searchable_pdf_dir, lang='vie+eng',
+                   initial_batch_size=10):
+    try:
+        # Extract the file name and create the corresponding directories
+        file_name = os.path.splitext(os.path.basename(pdf_path))[0]
+        processed_dir_path_before = os.path.join(processed_pdf_dir, "before", file_name).replace("\\", "/")
+        processed_dir_path_after = os.path.join(processed_pdf_dir, "after", file_name).replace("\\", "/")
+        extracted_dir_path = os.path.join(extracted_text_dir, file_name).replace("\\", "/")
 
-    # create the directories if they don't exist
-    os.makedirs(processed_dir_path_before, exist_ok=True)
-    os.makedirs(processed_dir_path_after, exist_ok=True)
-    os.makedirs(extracted_dir_path, exist_ok=True)
+        # create the directories if they don't exist
+        os.makedirs(processed_dir_path_before, exist_ok=True)
+        os.makedirs(processed_dir_path_after, exist_ok=True)
+        os.makedirs(extracted_dir_path, exist_ok=True)
 
-    pages = convert_from_path(pdf_path, thread_count=4, dpi=150, grayscale=True, hide_annotations=True)
-    print("Done converting to images!")
+        # Convert PDF pages to images
+        pages = convert_from_path(pdf_path, thread_count=4, dpi=150, grayscale=True, hide_annotations=True)
+        print("Done converting to images!")
 
-    # PDF merger
-    merger = PdfMerger()
+        # Get the total number of pages in the PDF
+        total_pages = len(pages)
 
-    # Set up a thread pool with an optimal number of workers
-    max_workers = psutil.cpu_count(logical=False) or 1  # Use physical cores if available, otherwise use 1
-    processing_successful = True  # Flag to indicate if processing was successful for all pages
-    with ThreadPoolExecutor(max_workers=max_workers) as executor:
-        try:
-            # Process each page using thread pool
-            pdf_page_paths = []
-            for page_num in range(len(pages)):
-                # Check CPU usage before processing each page
-                current_cpu_usage = get_cpu_usage()
-                if current_cpu_usage >= max_cpu_usage:
-                    print(f"CPU usage reached {current_cpu_usage}%, stopping processing.")
-                    processing_successful = False
-                    break
+        # Set up a thread pool with an optimal number of workers
+        max_workers = psutil.cpu_count(logical=False) or 1  # Use physical cores if available, otherwise use 1
+        with ThreadPoolExecutor(max_workers=max_workers) as executor:
+            pdf_page_paths = []  # List to store processed PDF pages
 
-                image = pages[page_num]
+            batch_size = initial_batch_size
 
-                # save the image
-                image_path = os.path.join(
-                    processed_dir_path_before, f"page_{page_num}.png")
-                image.save(image_path)  # Save the image as PNG
+            for batch_start in range(0, total_pages, batch_size):
+                batch_end = min(batch_start + batch_size, total_pages)
+                batch_pages = pages[batch_start:batch_end]
 
-                # check if the image size is larger than 178956970
-                total_pixels = image.size[0] * image.size[1]
+                # Process each page in the batch using thread pool
+                batch_futures = []
+                for page_num, image in enumerate(batch_pages, start=batch_start):
+                    # Check CPU usage before processing each page in the batch
+                    current_cpu_usage = get_cpu_usage()
+                    print(f"Current CPU usage: {current_cpu_usage}%")
+                    if current_cpu_usage >= max_cpu_usage:
+                        print(f"CPU usage reached {current_cpu_usage}%, reducing batch size for the current page.")
+                        batch_size = max(1, batch_size // 2)  # Reduce batch size by half for the current page
 
-                if total_pixels > 178956970:
-                    image = image.resize((width, height), Image.ANTIALIAS)
+                    future = executor.submit(preprocess_page, page_num, image, processed_dir_path_after,
+                                             extracted_dir_path)
+                    batch_futures.append(future)
 
-                future = executor.submit(preprocess_page, page_num, image, processed_dir_path_after,
-                                         extracted_dir_path)
-                pdf_page_paths.append(future)
-
-            # Wait for all PDF pages to be generated
-            pdf_page_paths = [future.result() for future in pdf_page_paths if future.result() is not None]
+                # Wait for all pages in the batch to be processed
+                batch_pdf_page_paths = [future.result() for future in batch_futures if future.result() is not None]
+                pdf_page_paths.extend(batch_pdf_page_paths)
 
             # Continue with PDF merging even if CPU usage threshold was exceeded
-            if processing_successful:
+            if pdf_page_paths:
                 # PDF merger
                 merger = PdfMerger()
                 for pdf_page_path in pdf_page_paths:
@@ -130,6 +127,7 @@ def preprocess_pdf(pdf_path, processed_pdf_dir, extracted_text_dir, searchable_p
                 merger.close()
                 print("Searchable PDF created successfully.")
             else:
-                print("Processing interrupted due to high CPU usage. Searchable PDF not created.")
-        except Exception as e:
-            print("Error in preprocess_pdf: ", e)
+                print("No pages were processed. Searchable PDF not created.")
+
+    except Exception as e:
+        print("Error in preprocess_pdf: ", e)
